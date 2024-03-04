70c461a87d8a04fbec860d0b35b82cc45d2fa9b9
Rickvkersbergen <R.van.kersbergen@hva.nl>
2024-03-04 10:45:40 +0100

    Created backend for the board

--- src/components/Board.ts
+++ src/components/Board.ts
@@ -1,96 +1,66 @@
 import {LitElement, css, html} from 'lit'
 import {customElement, property} from 'lit/decorators.js'
 import { ScenarioCardElement } from './ScenarioCard'
-import { RoleCardElement } from './RoleCard'
+import { RoleCardElement } from './RoleCard.ts'
+import { DeckElement } from './Decks'
+import { SpecialCardElement } from './SpecialCard.ts'
 
-@customElement('card-element')
-export abstract class CardElement extends LitElement{
-    protected readonly cardName: string
-    protected readonly context: string
-    protected readonly image: string
-    protected readonly cardType: CardType = CardType.ScenarioCard //Default value
-    protected _isFlipped: boolean = false
 
-    protected constructor(cardName: string, context: string, image: string, cardType: CardType) {
+@customElement('board-element')
+export class BoardElement extends LitElement{
+    private readonly _cardDecks: Array<DeckElement> = []
+    private readonly _players: Array<{"name": string, "card": SpecialCardElement | undefined}> = []
+    private readonly _roleCards: Array<RoleCardElement> = []
+    private readonly _specialCards: Array<ScenarioCardElement> = []
+
+
+    constructor(decks: Array<DeckElement>, players: Array<{"name": string, "card": SpecialCardElement | undefined}>, roleCards: Array<RoleCardElement>, specialCards: Array<ScenarioCardElement>) {
         super();
-        this.cardName = cardName;
-        this.context = context;
-        this.image = image;
-        this.cardType = cardType;
+        this._cardDecks = decks;
+        this._players = players;
+        this._roleCards = roleCards;
+        this._specialCards = specialCards;
     }
 
-    //Getters for the private properties
-    public get getCardName(): string {
-        return this.cardName;
+    public get getDecks(): Array<DeckElement> {
+        return this._cardDecks;
     }
 
-    public get getContext(): string {
-        return this.context;
+    public get getPlayers(): Array<{"name": string, "card": SpecialCardElement | undefined}> {
+        return this._players;
     }
 
-    public get getImage(): string {
-        return this.image;
+    public get getRoleCards(): Array<RoleCardElement> {
+        return this._roleCards;
     }
 
-    public get getIsFlipped(): boolean {
-        return this._isFlipped;
+    public get getSpecialCards(): Array<ScenarioCardElement> {
+        return this._specialCards;
     }
 
-    public get getCardType(): CardType {
-        return this.cardType;
+    assignSpecialCards(players: Array<{"name": string, "card": SpecialCardElement | undefined}>, deck: DeckElement) {
+        //Shuffle the deck
+        deck?.shuffle()
+        //Assign a special card to each player until the deck is empty
+        players.forEach(player => {
+            player.card = deck?.draw()
+        })
     }
 
-    protected flip() {
-        this._isFlipped = !this._isFlipped;
+    removeSpecialCards(players: Array<{"name": string, "card": SpecialCardElement | undefined}>, deck: DeckElement) {
+        players.forEach(player => {
+            deck?.push(player.card)
+            player.card = undefined
+        })
+    }
 
-        const front = this.shadowRoot?.querySelector('.card-front') ? this.shadowRoot?.querySelector('.card-front') : null
-        const rear = this.shadowRoot?.querySelector('.card-rear') ? this.shadowRoot?.querySelector('.card-rear') : null
-
-        if (this._isFlipped) {
-            front.style.display = 'none'
-            rear.style.display = 'flex'
-        } else {
-            front.style.display = 'flex'
-            rear.style.display = 'none'
-        }
+    render() {
+        return html`
+            
+        `
     }
 
     static styles = css`
-        .card {
-            display: flex;
-            flex-direction: column;
-            width: 10rem;
-            height: 16rem;
-            padding: 16px;
-            margin: 8px;
-            transition: transform 0.8s;
-            transform-style: preserve-3d;
-        }
-
-        .card-front {
-            display: flex;
-            flex-direction: column;
-            height: 100%;
-        }
-
-        .card-rear {
-            display: none;
-            flex-direction: column;
-            height: 100%;
-        }
-
-        .card img {
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-        }
-
-        .card-name {
-            text-align: center;
-            font-size: 1.5rem;
-            font-weight: bold;
-            margin-top: 8px;
-        }
-        
+       
     `
 }
