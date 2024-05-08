93cb155bc52248d5cbf447b7323e4527ed031ca9
Tabish <1631995@gmail.com>
2024-05-08 15:41:43 +0200

    implemented loading in from json

--- src/Root.ts
+++ src/Root.ts
@@ -8,9 +8,11 @@ import {RoleType} from "./enums/RoleType.ts";
 import {ScenarioCardElement} from "./components/ScenarioCard.ts";
 import {CardType} from "./enums/CardType.ts";
 import {ScenarioType} from "./enums/ScenarioType.ts";
-import { CurrentCardContainerElement } from './components/CurrentCardContainer.ts';
-import { ScenarioCardDeckElement } from './components/ScenarioCardDeck.ts';
-import { DiscardDeckElement } from './components/DiscardDeck.ts';
+import {CurrentCardContainerElement} from './components/CurrentCardContainer.ts';
+import {ScenarioCardDeckElement} from './components/ScenarioCardDeck.ts';
+import {DiscardDeckElement} from './components/DiscardDeck.ts';
+
+import scenarioCards from './assets/scenariocards.json'
 
 @customElement('root-element')
 export class RootElement extends LitElement {
@@ -43,62 +45,64 @@ export class RootElement extends LitElement {
     initDecks() {
         console.log("Initializing decks...")
         // init test decks red blue green yellow
+
         let redCards = new ScenarioCardDeckElement();
-        for (let i = 0; i < 5; i++) {
-            const card = new ScenarioCardElement(
-                "Test" + i,
-                "Test" + i,
-                "Test" + i,
-                CardType.ScenarioCard,
-                ScenarioType.RED,
-                ["1", "2"],
-                ["1", "2"]
-            );
-            redCards.push(card);
-            redCards.setDeckType = ScenarioType.RED;
-        }
         let blueCards = new ScenarioCardDeckElement();
-        for (let i = 0; i < 5; i++) {
-            const card = new ScenarioCardElement(
-                "Test" + i,
-                "Test" + i,
-                "Test" + i,
-                CardType.ScenarioCard,
-                ScenarioType.BLUE,
-                ["1", "2"],
-                ["1", "2"]
-            );
-            blueCards.push(card);
-            blueCards.setDeckType = ScenarioType.BLUE;
-        }
         let greenCards = new ScenarioCardDeckElement();
-        for (let i = 0; i < 5; i++) {
-            const card = new ScenarioCardElement(
-                "Test" + i,
-                "Test" + i,
-                "Test" + i,
-                CardType.ScenarioCard,
-                ScenarioType.GREEN,
-                ["1", "2"],
-                ["1", "2"]
-            );
-            greenCards.push(card);
-            greenCards.setDeckType = ScenarioType.GREEN;
-        }
         let yellowCards = new ScenarioCardDeckElement();
-        for (let i = 0; i < 5; i++) {
-            const card = new ScenarioCardElement(
-                "Test" + i,
-                "Test" + i,
-                "Test" + i,
-                CardType.ScenarioCard,
-                ScenarioType.YELLOW,
-                ["1", "2"],
-                ["1", "2"]
-            );
-            yellowCards.push(card);
-            yellowCards.setDeckType = ScenarioType.YELLOW;
-        }
+
+        scenarioCards.forEach((card: any) => {
+            switch (card.scenario) {
+                case ScenarioType.RED:
+                    redCards.push(new ScenarioCardElement(
+                        card.name,
+                        card.context,
+                        `./src/assets/card-images/${card.image}.png`,
+                        CardType.ScenarioCard,
+                        ScenarioType.RED,
+                        card.questions,
+                        card.tags
+                    ));
+                    redCards.setDeckType = ScenarioType.RED;
+                    break;
+                case ScenarioType.BLUE:
+                    blueCards.push(new ScenarioCardElement(
+                        card.name,
+                        card.context,
+                        `./src/assets/card-images/${card.image}.png`,
+                        CardType.ScenarioCard,
+                        ScenarioType.BLUE,
+                        card.questions,
+                        card.tags
+                    ));
+                    blueCards.setDeckType = ScenarioType.BLUE;
+                    break;
+                case ScenarioType.GREEN:
+                    greenCards.push(new ScenarioCardElement(
+                        card.name,
+                        card.context,
+                        `./src/assets/card-images/${card.image}.png`,
+                        CardType.ScenarioCard,
+                        ScenarioType.GREEN,
+                        card.questions,
+                        card.tags
+                    ));
+                    greenCards.setDeckType = ScenarioType.GREEN;
+                    break;
+                case ScenarioType.YELLOW:
+                    yellowCards.push(new ScenarioCardElement(
+                        card.name,
+                        card.context,
+                        `./src/assets/card-images/${card.image}.png`,
+                        CardType.ScenarioCard,
+                        ScenarioType.YELLOW,
+                        card.questions,
+                        card.tags
+                    ));
+                    yellowCards.setDeckType = ScenarioType.YELLOW;
+                    break;
+            }
+        });
 
         return [redCards, blueCards, greenCards, yellowCards];
     }
